26a6dc1001cb6d0a915d2c55fea2170e4f38c264
kevchang <kevchang@cs.stonybrook.edu>
2018-12-01 20:43:26 -0500

    preprocessing

--- src/main/java/app/gerry/AlgorithmCore/SimulatedAnnealing.java
+++ src/main/java/app/gerry/AlgorithmCore/SimulatedAnnealing.java
@@ -4,12 +4,15 @@ import app.gerry.Geography.Chunk;
 import app.gerry.Geography.District;
 import app.gerry.Geography.State;
 import app.gerry.Sse.SseResultData;
+import java.util.HashSet;
 import java.util.Set;
 
 public class SimulatedAnnealing implements Algorithm{
     State startingState;
-    public SimulatedAnnealing(State state){
+    MoveManager mm;
+    public SimulatedAnnealing(State state,MoveManager manager){
         startingState = state;
+        mm = manager;
     }
     
     @Override
@@ -21,12 +24,16 @@ public class SimulatedAnnealing implements Algorithm{
         Chunk movingChunk = srcDistrict.getRandomBorderChunk();
         //3) Get the Districts adjacent to the srcDistrict from step 1: destDistricts
         Set<Chunk> chunks = movingChunk.getAdjacentChunks();
-        
+        HashSet<District> destDistricts = new HashSet<>();
         for(Chunk ck: chunks){
-            ck.getDistrict();
+            District temp = ck.getDistrict();
+            if(temp != srcDistrict && !destDistricts.contains(temp)){
+                destDistricts.add(ck.getDistrict());
+            }   
         }
-        
         //4) createAllMoves(chunk,srcDistrict,destDistricts): array[moves]
+        mm.createAllMoves(movingChunk, srcDistrict, destDistricts);
+        
         //5) Iterate through all moves until you find a move that increases objective
         //6) finalize that move. 
         
