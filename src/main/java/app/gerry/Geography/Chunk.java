7007907347d20f7aca7e08724daac606e2fc3faf
TZHENG-PC\Tina <tina.zheng.1@stonybrook.edu>
2018-12-03 23:00:25 -0500

    I just copied and pasted what's in chunk on the main branch

--- src/main/java/app/gerry/Geography/Chunk.java
+++ src/main/java/app/gerry/Geography/Chunk.java
@@ -2,38 +2,145 @@ package app.gerry.Geography;
 
 import app.gerry.Data.ElectionData;
 import app.gerry.Data.GeometricData;
-import app.gerry.Enum.PoliticalSubdivision;
+import app.gerry.Constants.PoliticalSubdivision;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.Set;
 
 public class Chunk {
-    public int id;
-    public String name;
-    public Set<Precinct> precincts;
-    public Set<Chunk> adjacentChunks;
-    public ElectionData cummElectionData;
-    public GeometricData cummGeometricData;
-    public int cummPopulation;
-    public PoliticalSubdivision subdivision;
-    public boolean isFinalized;
-    public District parentDistrict;
-    public boolean isBorderChunk;
+    private int id;
+    private String name;
+    private List<Precinct> precincts;
+    private List<Chunk> adjacentChunks;
+    private ElectionData cummElectionData;
+    private GeometricData cummGeometricData;
+    private int cummPopulation;
+    private PoliticalSubdivision subdivision;
+    private boolean isFinalized;
+    private District parentDistrict;
+    private boolean isBorderChunk;
+
+    /**
+     * Chunk of one precinct
+     * @param precinct
+     */
+    public Chunk(Precinct precinct) {
+        precincts = new ArrayList<>();
+        precincts.add(precinct);
+        subdivision = PoliticalSubdivision.PRECINCT;
+        id = precinct.getId();
+        //TODO: fill in rest of fields
+    }
+
+    /**
+     * Chunk of many precincts eg. county
+     * @param precincts
+     */
+    public Chunk(List<Precinct> precincts) {
+        precincts.addAll(precincts);
+        subdivision = PoliticalSubdivision.COUNTY;
+        //TODO: assign county id to chunk id
+        //TODO: fill in rest of fields
+    }
 
     //I don't think this would work logically
-    /*public Set<District> getAdjacentDistricts(){
+    /*public Set<DistrictEntity> getAdjacentDistricts(){
         return null;
     }*/
-
-
     public District getDistrict(){
         return parentDistrict;
     }
-    
+
     public Set<Chunk> getAdjacentChunks(){
-        return adjacentChunks;
+        return null;
     }
     public void populateChunk(Precinct p){
         return;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Precinct> getPrecincts() {
+        return precincts;
+    }
+
+    public void setPrecients(List<Precinct> precincts) {
+        this.precincts = precincts;
+    }
+
+    public void setAdjacentChunks(List<Chunk> adjacentChunks) {
+        this.adjacentChunks = adjacentChunks;
+    }
+
+    public ElectionData getCummElectionData() {
+        return cummElectionData;
+    }
+
+    public void setCummElectionData(ElectionData cummElectionData) {
+        this.cummElectionData = cummElectionData;
+    }
+
+    public GeometricData getCummGeometricData() {
+        return cummGeometricData;
+    }
+
+    public void setCummGeometricData(GeometricData cummGeometricData) {
+        this.cummGeometricData = cummGeometricData;
+    }
+
+    public int getCummPopulation() {
+        return cummPopulation;
+    }
+
+    public void setCummPopulation(int cummPopulation) {
+        this.cummPopulation = cummPopulation;
+    }
+
+    public PoliticalSubdivision getSubdivision() {
+        return subdivision;
+    }
+
+    public void setSubdivision(PoliticalSubdivision subdivision) {
+        this.subdivision = subdivision;
+    }
+
+    public boolean isFinalized() {
+        return isFinalized;
+    }
+
+    public void setFinalized(boolean finalized) {
+        isFinalized = finalized;
+    }
+
+    public District getParentDistrict() {
+        return parentDistrict;
+    }
+
+    public void setParentDistrict(District parentDistrict) {
+        this.parentDistrict = parentDistrict;
+    }
+
+    public boolean isBorderChunk() {
+        return isBorderChunk;
+    }
+
+    public void setBorderChunk(boolean borderChunk) {
+        isBorderChunk = borderChunk;
     }
 }
