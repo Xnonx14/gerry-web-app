5ed7328de3e5c1bbfebd15ef5bd3b8edf7546639
unknown <zjackiew@gmail.com>
2018-12-03 23:34:33 -0500

    POM

---
+++ src/main/resources/static/js/changeColor.js
@@ -0,0 +1,18 @@
+//Call Script
+//<script id="changeColor" src="js/changeColor.js" precinct = ".." district = "..">/script>
+
+//var precinct = $('#changeColor').attr("precinct");
+//var district = $('#changeColor').attr("district");
+
+//function colorStyle(color){
+//    return {
+//    		fillColor: color,
+//    		fillOpacity: 0.3,
+//    		stroke: true,
+//    		fill: true,
+//    		color: 'black',
+//    		weight: 1,
+//    };
+//}
+//var color = genColor(district);
+//district.setFeatureStyle(precinct, colorStyle(color));
